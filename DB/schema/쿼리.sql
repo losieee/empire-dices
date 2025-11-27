cf25bc0506437182089fe54a6a04d479088bb8c9
shingumingi <mingi3is@g.shingu.ac.kr>
2025-11-27 16:15:13 +0900

    [Feat] 맵생성

---
+++ DB/schema/쿼리.sql
@@ -0,0 +1,14 @@
+INSERT INTO country (country_name, country_grade, country_flag_image) VALUES
+('미국', '강', 'US.png'),
+('중국', '강', 'CN.png'),
+('러시아', '강', 'RU.png'),
+('한국', '강', 'KR.png'),
+
+('북한', '약', 'KP.png'),
+('몽골', '약', 'MN.png'),
+('소말리아', '약', 'SO.png'),
+('이라크', '약', 'IQ.png'),
+('이란', '약', 'IR.png'),
+('쿠바', '약', 'CU.png'),
+('몰디브', '약', 'MV.png'),
+('아프가니스탄', '약', 'AF.png');
